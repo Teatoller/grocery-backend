3a7e51a76139088e1d979d85d1fa22cf95a18452
Steven Ennis <steven.otieno1@gmail.com>
2021-06-13 14:48:54 +0300

    chore(project set up): implement project set up
    - set up project
    - add product
    
    [Starts #001]

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '84212a11d6605aa0a8f973fc2a7fb767'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eb9ea3e1-0869-4653-93db-76c2092dcac0'
+};
