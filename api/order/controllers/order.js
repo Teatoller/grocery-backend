296c683d889855750dafd919e5bba7a72a74f839
Steven Ennis <steven.otieno1@gmail.com>
2021-06-16 21:36:55 +0300

    feature(stripe): implement stripe
    - install stripe
    - add stripe key
    - add create method on order controller
    - add methed to generate session
    - add new order method
    - add confirmation method
    - add confirmation route for testion
    
    [Finishes #005]

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,5 +1,13 @@
 "use strict";
 const { sanitizeEntity } = require("strapi-utils");
+const stripe = require("stripe")(process.env.STRIPE_SK);
+
+/**
+ * Given the  dollar amount, return the amount in cents
+ * @param {number} number
+ * @returns
+ */
+const fromDecimalToInt = (number) => parseInt(number * 100);
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -44,5 +52,83 @@ module.exports = {
 
     const entity = await strapi.services.order.findOne({ id, user: user.id });
     return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+  /**
+   * Create an order and set up the Stripe checkout session for the frontend.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    const { product } = ctx.request.body;
+
+    if (!product) {
+      return ctx.throw(400, "Please specify a product");
+    }
+
+    const realProduct = await strapi.services.product.findOne({
+      id: product.id,
+    });
+
+    if (!realProduct) {
+      return ctx.throw(404, "No product with such id");
+    }
+
+    const { user } = ctx.state;
+    const BASE_URL = ctx.request.headers.origin || "http://localhost:3000";
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      customer_email: user.email,
+      mode: "payment",
+      success_url: `${BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: BASE_URL,
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: realProduct.name,
+            },
+            unit_amount: fromDecimalToInt(realProduct.price),
+          },
+          quantity: 1,
+        },
+      ],
+    });
+    // Create the order
+    const newOrder = await strapi.services.order.create({
+      user: user.id,
+      product: realProduct.id,
+      total: realProduct.price,
+      status: "unpaid",
+      checkout_session: session.id,
+    });
+
+    return { id: session.id };
+  },
+  // Confirm
+  /**
+   * Given a checkout session, verifies payment and updates the order
+   * @param {any} ctx
+   * @returns
+   */
+  async confirm(ctx) {
+    const { checkout_session } = ctx.request.body;
+    const session = await stripe.checkout.sessions.retrieve(checkout_session);
+
+    if (session.payment_status === "paid") {
+      const updateOrder = await strapi.services.order.update(
+        {
+          checkout_session,
+        },
+        {
+          status: "paid",
+        }
+      );
+      return sanitizeEntity(updateOrder, { model: strapi.models.order });
+    } else {
+      ctx.throw(400, "The payment was not successful, please call support");
+    }
   },
 };
